52fbda2ed6a8b647007a2243dbf25d99729fb0cd
Bruno Finger <bruno.k.finger@gmail.com>
2023-11-10 09:57:07 +0100

    #26 Properly refer to ~/.local/share/fonts in comments

--- util/install_linux.sh
+++ util/install_linux.sh
@@ -3,10 +3,10 @@
 # remove all fonts from ~/.local/share/fonts that start with "Monaspace"
 rm -rf ~/.local/share/fonts/Monaspace*
 
-# copy all fonts from ./otf to ~/Library/Fonts
+# copy all fonts from ./otf to ~/.local/share/fonts
 cp ./fonts/otf/* ~/.local/share/fonts
 
-# copy variable fonts from ./variable to ~/Library/Fonts
+# copy variable fonts from ./variable to ~/.local/share/fonts
 cp ./fonts/variable/* ~/.local/share/fonts
 
 # Build font information caches
