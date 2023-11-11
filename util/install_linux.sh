4c5d4c903bb45a6718f6467def708a339c995261
Mayank Shekhar <22864222+shekharmayank@users.noreply.github.com>
2023-11-11 14:00:55 +0530

    fixes #63

--- util/install_linux.sh
+++ util/install_linux.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# create fonts dir if not present
+mkdir -p ~/.local/share/fonts
+
 # remove all fonts from ~/.local/share/fonts that start with "Monaspace"
 rm -rf ~/.local/share/fonts/Monaspace*
 
-# copy all fonts from ./otf to ~/.local/share/fonts
-cp ./fonts/otf/* ~/.local/share/fonts
+# get dir of this script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# copy variable fonts from ./variable to ~/.local/share/fonts
-cp ./fonts/variable/* ~/.local/share/fonts
+# copy all fonts from monaspace root/fonts/otf to ~/.local/share/fonts
+cp $SCRIPT_DIR/../fonts/otf/* ~/.local/share/fonts
+
+# copy variable fonts from monaspace root/fonts/variable to ~/.local/share/fonts
+cp $SCRIPT_DIR/../fonts/variable/* ~/.local/share/fonts
 
 # Build font information caches
 fc-cache -f
