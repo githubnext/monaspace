dca08ca52c9000e5ef739cc3afcfbaf7f2f36a49
Abel Jimenez <abelj1@uci.edu>
2023-11-10 20:38:57 -0800

    updated linux install script to fix readme and folder not existing

--- util/install_linux.sh
+++ util/install_linux.sh
@@ -3,11 +3,14 @@
 # remove all fonts from ~/.local/share/fonts that start with "Monaspace"
 rm -rf ~/.local/share/fonts/Monaspace*
 
+# make the folder if it does not exist
+mkdir -p ~/.local/share/fonts 
+
 # copy all fonts from ./otf to ~/.local/share/fonts
-cp ./fonts/otf/* ~/.local/share/fonts
+cp ../fonts/otf/* ~/.local/share/fonts
 
 # copy variable fonts from ./variable to ~/.local/share/fonts
-cp ./fonts/variable/* ~/.local/share/fonts
+cp ../fonts/variable/* ~/.local/share/fonts
 
 # Build font information caches
 fc-cache -f
