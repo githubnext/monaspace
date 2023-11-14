b064a632b116b15131035e8ef60d979ebcaf9c8a
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-11-14 16:04:23 +0700

    update install_linux.sh

--- util/install_linux.sh
+++ util/install_linux.sh
@@ -3,13 +3,13 @@
 # remove all fonts from ~/.local/share/fonts that start with "Monaspace"
 rm -rf ~/.local/share/fonts/Monaspace*
 
-mkdir -p ~/.local/share/fonts
+mkdir -p ~/.local/share/fonts/Monaspace/
 
 # copy all fonts from ./otf to ~/.local/share/fonts
-cp ./fonts/otf/* ~/.local/share/fonts
+cp ./fonts/otf/* ~/.local/share/fonts/Monaspace/
 
 # copy variable fonts from ./variable to ~/.local/share/fonts
-cp ./fonts/variable/* ~/.local/share/fonts
+cp ./fonts/variable/* ~/.local/share/fonts/Monaspace/
 
 # Build font information caches
 fc-cache -f
