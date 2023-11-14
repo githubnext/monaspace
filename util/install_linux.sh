1e467616b395d3ddda0369e34bfe169f43192a21
Muhammad Atif Ali <me@matifali.dev>
2023-11-14 14:45:33 +0300

    fix linux install script

--- util/install_linux.sh
+++ util/install_linux.sh
@@ -3,11 +3,14 @@
 # remove all fonts from ~/.local/share/fonts that start with "Monaspace"
 rm -rf ~/.local/share/fonts/Monaspace*
 
+# Create the directory ~/.local/share/fonts if it doesn't exist
+mkdir -p ~/.local/share/fonts
+
 # copy all fonts from ./otf to ~/.local/share/fonts
-cp ./fonts/otf/* ~/.local/share/fonts
+cp ../fonts/otf/* ~/.local/share/fonts
 
 # copy variable fonts from ./variable to ~/.local/share/fonts
-cp ./fonts/variable/* ~/.local/share/fonts
+cp ../fonts/variable/* ~/.local/share/fonts
 
 # Build font information caches
 fc-cache -f
