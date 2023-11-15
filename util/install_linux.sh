350311f86d0c25ffeb9e1c2c51f4ac324a79866c
Charles Vallance <vallance.charles@gmail.com>
2023-11-16 11:29:54 +1300

    Fixing install_linux.sh script with correct path and also creation of the users local font directory

--- util/install_linux.sh
+++ util/install_linux.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Create the users local fonts folder if it doesn't exist
+mkdir -p ~/.local/share/fonts
+
 # remove all fonts from ~/.local/share/fonts that start with "Monaspace"
 rm -rf ~/.local/share/fonts/Monaspace*
 
 # copy all fonts from ./otf to ~/.local/share/fonts
-cp ./fonts/otf/* ~/.local/share/fonts
+cp ../fonts/otf/* ~/.local/share/fonts
 
 # copy variable fonts from ./variable to ~/.local/share/fonts
-cp ./fonts/variable/* ~/.local/share/fonts
+cp ../fonts/variable/* ~/.local/share/fonts
 
 # Build font information caches
 fc-cache -f
