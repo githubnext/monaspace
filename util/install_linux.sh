708844556d7e849f7a48854357f75a8ea76d64d1
Bruno Finger <bruno.k.finger@gmail.com>
2024-02-02 10:19:31 +0100

    #185 Update Linux install script to refer to the correct relative paths

--- util/install_linux.sh
+++ util/install_linux.sh
@@ -4,10 +4,10 @@
 rm -rf ~/.local/share/fonts/Monaspace*
 
 # copy all fonts from ./otf to ~/.local/share/fonts
-cp ./fonts/otf/* ~/.local/share/fonts
+cp ../fonts/otf/* ~/.local/share/fonts
 
 # copy variable fonts from ./variable to ~/.local/share/fonts
-cp ./fonts/variable/* ~/.local/share/fonts
+cp ../fonts/variable/* ~/.local/share/fonts
 
 # Build font information caches
 fc-cache -f
