ddf438aa4f2c20d4cb3ff33326290c588be21388
Olivier Patry <olivier.patry@gmail.com>
2024-11-04 09:41:45 +0100

    Extract in/out variables for convenience in install scripts
    Would easily allow letting caller choose output dir for instance.
    
    ```bash
    fonts_dir="${1:-"${HOME}/Library/Fonts"}"
    ```
    
    Note:
    Used `${HOME}` as a replacement of `~` which doesn't expands within quotes.
    See https://www.shellcheck.net/wiki/SC2088

--- util/install_macos.sh
+++ util/install_macos.sh
@@ -4,11 +4,14 @@ set -euo pipefail
 
 origin=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) || exit
 
+input_dir="${origin}/../fonts"
+fonts_dir="${HOME}/Library/Fonts"
+
 # remove all fonts from ~/Library/Fonts that start with "Monaspace"
-rm -rf ~/Library/Fonts/Monaspace*
+rm -rf "${fonts_dir}"/Monaspace*
 
 # copy all fonts from ./otf to ~/Library/Fonts
-cp "${origin}/../fonts/otf/"* ~/Library/Fonts
+cp "${input_dir}/otf/"* "${fonts_dir}"
 
 # copy variable fonts from ./variable to ~/Library/Fonts
-cp "${origin}/../fonts/variable/"* ~/Library/Fonts
+cp "${input_dir}/variable/"* "${fonts_dir}"
