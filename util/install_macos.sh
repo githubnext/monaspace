8ea06897928da75ade22f53833e9a77a428a20f1
Idan Gazit <idan@gazit.me>
2024-12-20 11:12:37 -0800

    Update installer scripts to handle frozen fonts

--- util/install_macos.sh
+++ util/install_macos.sh
@@ -3,8 +3,11 @@
 # remove all fonts from ~/Library/Fonts that start with "Monaspace"
 rm -rf ~/Library/Fonts/Monaspace*
 
-# copy all fonts from ./otf to ~/Library/Fonts
+# copy static fonts from ./otf to ~/Library/Fonts
 cp ./fonts/otf/* ~/Library/Fonts
 
 # copy variable fonts from ./variable to ~/Library/Fonts
 cp ./fonts/variable/* ~/Library/Fonts
+
+# copy frozen fonts from ./frozen to ~/Library/Fonts
+cp ./fonts/frozen/* ~/Library/Fonts
